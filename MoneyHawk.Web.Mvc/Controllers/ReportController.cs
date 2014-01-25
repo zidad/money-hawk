9e980a492f1bca6d72bbd447ab11478876e8515c
Wiebe Tijsma <wiebe@tijsma.com>
2014-01-25 16:10:58 +0100

    added excel tools

--- MoneyHawk.Web.Mvc/Controllers/ReportController.cs
+++ MoneyHawk.Web.Mvc/Controllers/ReportController.cs
@@ -1,12 +1,13 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Drawing;
 using System.Linq;
 using System.Web.Helpers;
 using System.Web.Mvc;
-using Microsoft.AspNet.Identity;
 using MoneyHawk.Core;
-using MoneyHawk.Web.Mvc.Models;
 using NetIndustry.Library;
+using OfficeOpenXml;
+using OfficeOpenXml.Style;
 
 namespace MoneyHawk.Web.Mvc.Controllers
 {
@@ -46,13 +47,64 @@ namespace MoneyHawk.Web.Mvc.Controllers
             var contacts = this.api.Contacts.GetAll();
             return contacts;
         }
+
+        [HttpPost]
+        public ActionResult Export(ReportOptions options)
+        {
+            using (var pck = new ExcelPackage())
+            {
+/*
+                //Create the worksheet
+                ExcelWorksheet ws = pck.Workbook.Worksheets.Add("Demo");
+
+                //Load the datatable into the sheet, starting from cell A1. Print the column names on row 1
+                ws.Cells["A1"].LoadFromDataTable(tbl, true);
+
+                //Format the header for column 1-3
+                using (ExcelRange rng = ws.Cells["A1:C1"])
+                {
+                    rng.Style.Font.Bold = true;
+                    rng.Style.Fill.PatternType = ExcelFillStyle.Solid;                      //Set Pattern for the background to Solid
+                    rng.Style.Fill.BackgroundColor.SetColor(Color.FromArgb(79, 129, 189));  //Set color to dark blue
+                    rng.Style.Font.Color.SetColor(Color.White);
+                }
+
+                //Example how to Format Column 1 as numeric 
+                using (ExcelRange col = ws.Cells[2, 1, 2 + tbl.Rows.Count, 1])
+                {
+                    col.Style.Numberformat.Format = "#,##0.00";
+                    col.Style.HorizontalAlignment = ExcelHorizontalAlignment.Right;
+                }
+
+                //Write it back to the client
+                Response.ContentType = "application/vnd.openxmlformats-officedocument.spreadsheetml.sheet";
+                Response.AddHeader("content-disposition", "attachment;  filename=ExcelDemo.xlsx");
+                Response.BinaryWrite(pck.GetAsByteArray());
+
+                return new FileStreamResult();
+                //return new ExcelExportResult();
+*/
+                throw new NotImplementedException();
+            }
+        }
+    }
+
+    public class ExcelExportResult : ActionResult
+    {
+        public override void ExecuteResult(ControllerContext context)
+        {
+            
+        }
+    }
+
+    public class ReportOptions
+    {
     }
 
     public class ExpenseReport 
     { 
         
     }
-
 
     public class ExpenseLine
     {
@@ -110,7 +162,7 @@ namespace MoneyHawk.Web.Mvc.Controllers
             {
                 wrappedFormat = o => format((T)o.Value);
             }
-            WebGridColumn column = Column(columnName, header, wrappedFormat, style, canSort);
+            var column = Column(columnName, header, wrappedFormat, style, canSort);
             return column;
         }
 
